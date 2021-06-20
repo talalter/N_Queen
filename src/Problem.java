c17c708217a0e417546e10b39945128d42a369c6
talalter <64363268+talalter@users.noreply.github.com>
2021-06-20 23:46:19 +0300

    Update Problem.java

--- src/Problem.java
+++ src/Problem.java
@@ -1,6 +1,3 @@
-// Tal alter 206292450
-// Or abitbol 312614605
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -12,39 +9,36 @@ public class Problem {
 
     // example for path input:   C:/Users/talal/Downloads/Ass1array.txt
     public static void main(String[] args) throws FileNotFoundException {
-//        Scanner input = new Scanner(System.in);
-//        System.out.println("Enter the path C:/Username.../array.txt:");
-//        String path = input.nextLine();
-//        Scanner sc = new Scanner(new BufferedReader(new FileReader(path)));
-//        System.out.println("Enter the size of matrix");
-//        int size = input.nextInt();
-//        System.out.println("Enter the starting Node state");
-//        char start_char = input.next().charAt(0);
-//        Node start = new Node((int) start_char, null, 0, 0);
-//        System.out.println("Enter the ending Node state");
-//        char end_char = input.next().charAt(0);
-//        Node end = new Node((int) end_char, null, 0, 0);
-//        int[][] myArray = new int[size][size];
-//        while (sc.hasNextLine()) {
-//            for (int i = 0; i < size; i++) {
-//                String[] line = sc.nextLine().trim().split(",");
-//                for (int j = 0; j < size; j++) {
-//                    myArray[i][j] = Integer.parseInt(line[j]);
-//                }
-//            }
-//        }
-//        Algorithm alg = new Algorithm(myArray,start,end);
-//        alg.setSearchType(new DFS(myArray,start,end));
-//        alg.search();
-//        alg.setSearchType(new BFS(myArray,start,end));
-//        alg.search();
-//        alg.setSearchType(new UCS(myArray,start,end));
-//        alg.search();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the path C:/Username.../array.txt:");
+        String path = input.nextLine();
+        Scanner sc = new Scanner(new BufferedReader(new FileReader(path)));
+        System.out.println("Enter the size of matrix");
+        int size = input.nextInt();
+        System.out.println("Enter the starting Node state");
+        char start_char = input.next().charAt(0);
+        Node start = new Node((int) start_char, null, 0, 0);
+        System.out.println("Enter the ending Node state");
+        char end_char = input.next().charAt(0);
+        Node end = new Node((int) end_char, null, 0, 0);
+        int[][] myArray = new int[size][size];
+        while (sc.hasNextLine()) {
+            for (int i = 0; i < size; i++) {
+                String[] line = sc.nextLine().trim().split(",");
+                for (int j = 0; j < size; j++) {
+                    myArray[i][j] = Integer.parseInt(line[j]);
+                }
+            }
+        }
+        Algorithm alg = new Algorithm(myArray,start,end);
+        alg.setSearchType(new DFS(myArray,start,end));
+        alg.search();
+        alg.setSearchType(new BFS(myArray,start,end));
+        alg.search();
+        alg.setSearchType(new UCS(myArray,start,end));
+        alg.search();
 
-        Random ran = new Random();
-        for(int i=0;i<15;i++) {
-            int x = ran.nextInt(4) + 0;
-            System.out.println(x);
+
         }
     }
 }
